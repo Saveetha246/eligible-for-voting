9a5ec71c05a7789ef3adacae12aa20bccc2531f6
Saveetha246 <102456627+Saveetha246@users.noreply.github.com>
2022-05-30 14:18:30 +0530

    program to check eligibility criteria
    this file checks if a person is eligible to vote or not

---
+++ eligibility for voting.c
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int age;
+cout<<"ENTER THE AGE:";
+cin>>age;
+if(age>=18)
+{
+cout<<"ELIGIBLE FOR VOTING";
+}
+else
+{
+cout<<"NOT ELIGIBLE FOR VOTING";
+}
+return 0;
+}
